4ff651ba642d34bccaa9dcbcee1c19b7ecdd4ca8
pogu95 <8yd3@fresnou.org>
2022-03-18 12:55:09 -0700

    Final commit

---
+++ models/product.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.hasMany(models.Review, {
+        foreignKey:'product_id',
+        as: 'reviews'
+      })
+    }
+  };
+  Product.init({
+    product_name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    description: DataTypes.STRING,
+    rating:{
+      type:DataTypes.VIRTUAL,
+      get(){
+        const reviewCount = this.reviews.length;
+        if (!reviewCount){
+          return 0;
+        }
+        let total = 0;
+        for (let i=0; i<reviewCount; i++){
+          total+= this.reviews[i].rating;
+        }
+        return total/reviewCount;
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Product',
+    timestamps:false,
+    tableName: 'products'
+  });
+  return Product;
+};
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const productController = require('../controllers/productControllers.js');
+const reviewController = require('../controllers/reviewControllers.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('/products');
+});
+router.get('/products', productController.viewProducts);
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
+router.post('/products/:productId/review/add', reviewController.addReview);
+router.get('/reviews/edit/:id', reviewController.renderEditReviewForm);
+router.post('/reviews/edit/:id', reviewController.updateReview);
+router.get('/reviews/delete/:id', reviewController.deleteReview);
+module.exports = router;
